bb2aad7574eef5b00cba1bfd59f686dd1d6d0eb3
chinyunchen <chenclaire17@gmail.com>
2019-06-22 23:04:43 +0800

    update App.js and add ErrorBoundary.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import { setSearchField, requestRobots } from '../actions';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+// parameter state comes from index.js provider store state(rootReducers)
 const mapStateToProps = (state) => {
 	return {
 		searchField: state.searchRobots.searchField,
@@ -15,6 +17,8 @@ const mapStateToProps = (state) => {
 	}
 }
 
+// dispatch the DOM changes to call an action. mapDispatchToProps returns a function 
+// that returns an object then uses connect to change the data form reducers.
 const mapDispatchToProps = (dispatch) => {
 	return {
 		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
@@ -22,6 +26,7 @@ const mapDispatchToProps = (dispatch) => {
 	}
 }
 
+// because uses Redux to manage states there is no need to use this.state and this.setState
 class App extends Component {
 	// constructor() {
 	// 	super()
@@ -48,19 +53,22 @@ class App extends Component {
 		const filteredRobots = robots.filter(robot=> {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
-		return isPending ?
-			<h1> Loading </h1> :
-			(
-				<div className = 'tc'>
-					<h1 className = 'f1'>RoboFriends</h1>
-					<SearchBox searchChange = { onSearchChange }/>
-					<Scroll>
+		return (
+			<div className = 'tc'>
+				<h1 className = 'f1'>RoboFriends</h1>
+				<SearchBox searchChange = { onSearchChange }/>
+				<Scroll>
+				{ isPending ? <h1> Loading </h1> :
+					<ErrorBoundary>
 						<CardList robots = { filteredRobots }/>
-					</Scroll>
-				</div>
-			);
+					</ErrorBoundary>
+				}	
+				</Scroll>
+			</div>
+		);
 	}
 }
 
-// connect() is a higher order function
+// connect() is a higher order function, action done from mapDispatchToProps will 
+// change state from mapStateToProps
 export default connect(mapStateToProps, mapDispatchToProps)(App);
